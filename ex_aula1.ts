1e17a6cf8096d6cffdfeb80237be54d3e41c6b40
Italo <igivas26@gmail.com>
2021-02-28 02:34:58 -0300

    projeto final nlw4

---
+++ ex_aula1.ts
@@ -0,0 +1,12 @@
+type User = {
+    name: string;
+    age: string;
+    address: {
+      city: string;
+      uf: string;
+    }
+}
+
+function showUserInfo(user: User){
+   return ´Welcome, ${user.name} - ${user.age}. City: ${user.address.city}, UF: ${user.address.uf}´;
+}
